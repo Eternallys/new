b8efdea1a8cde5a1ea9296682a37e499c6256cc0
Eternallys <43713058+Eternallys@users.noreply.github.com>
2019-05-16 10:42:25 +0800

    Add files via upload

---
+++ qianggeing/9.js
@@ -0,0 +1,14 @@
+window.onload = function (){
+  console.log (WhatEver);
+  var WhatEver=document.getElementById ('amao')
+  console.log (WhatEver)
+  console.log ('aa'*1)
+  console.log (0/100)
+  console.log (typeof(100))
+  console.log (typeof('100'))
+  if (100==='100'){
+    document.write(typeof(100))
+  } else {
+    document.write(typeof('100'))
+  }
+}
---
+++ qianggeing/a.js
@@ -0,0 +1,6 @@
+function init (){
+  var divGet=document.getElementById('agow')
+  divGet.innerHTML=''
+  divGet.setAttribute('style','height:100px ; width:500px ; background-color:yellow ; position:relative ; top:200px ; left:500px')
+}
+window.onload=init
